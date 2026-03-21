813ae92c1102162ccaedb70366093954c713af95
sudeshsudhii <sudeshtiger1999@gmail.com>
2026-03-21 14:40:00 +0530

    feat: add verification response model

---
+++ src/main/java/com/eventchain/dto/VerifyResponse.java
@@ -0,0 +1,20 @@
+package com.eventchain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyResponse {
+    private boolean exists;
+    private BigInteger index;
+    private String message;
+}
+
+// TODO: fix typo in field
