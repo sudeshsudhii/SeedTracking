3fc8297eaf53a676ff74c5f77e1fd0b678f8e74a
sudeshsudhii <sudeshtiger1999@gmail.com>
2026-06-01 09:00:00 +0530

    feat: implement event verification pages

---
+++ frontend/src/pages/VerifyEvent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import VerifyEventComponent from '../components/VerifyEventComponent';
+
+const VerifyEvent = () => {
+  return <VerifyEventComponent />;
+};
+
+export default VerifyEvent;
---
+++ frontend/src/pages/VerifyPage.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import CertificateVerifier from '../components/CertificateVerifier';
+const VerifyPage = () => <CertificateVerifier />;
+export default VerifyPage;
